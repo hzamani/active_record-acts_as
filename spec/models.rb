7bbca78bcc086a8b624ccfa06bc5b5b9bdea9824
Rob Jacoby <rjacoby@localdirectories.com.au>
2014-07-31 15:38:58 +1000

    add namespaced models

--- spec/models.rb
+++ spec/models.rb
@@ -21,6 +21,24 @@ class Store < ActiveRecord::Base
   has_many :products
 end
 
+module Inventory
+end
+
+class Inventory::ProductFeature < ActiveRecord::Base
+  actable
+  validates_presence_of :name, :price
+
+  def present
+    "#{name} - $#{price}"
+  end
+end
+
+class Inventory::PenLid < ActiveRecord::Base
+  acts_as :product_feature
+
+  validates_presence_of :color
+end
+
 initialize_database do
   create_table :pens do |t|
     t.string :color
@@ -35,5 +53,15 @@ initialize_database do
 
   create_table :stores do |t|
     t.string :name
+  end
+
+  create_table :inventory_pen_lids do |t|
+    t.string :color
+  end
+
+  create_table :inventory_product_features do |t|
+    t.string :name
+    t.float :price
+    t.actable
   end
 end
