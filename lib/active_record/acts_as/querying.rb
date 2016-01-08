2d1650532843aa591f83ad0521c98f1ac8b22e72
Jorge Aguirre <jorge.aguirre.sza@gmail.com>
2016-01-08 16:58:15 -0300

    add rails 5.0 support

--- lib/active_record/acts_as/querying.rb
+++ lib/active_record/acts_as/querying.rb
@@ -1,17 +1,16 @@
 module ActiveRecord
-  class Relation
-        def scope_for_create_with_acts_as
+  module ScopeForCreateWithActAs
+    def scope_for_create
       @scope_for_create ||= if acting_as?
-        where_values_hash.merge(where_values_hash(acting_as_model.table_name)).merge(create_with_value)
-      else
-        where_values_hash.merge(create_with_value)
-      end
+                              where_values_hash.merge(where_values_hash(acting_as_model.table_name)).merge(create_with_value)
+                            else
+                              where_values_hash.merge(create_with_value)
+                            end
     end
-    alias_method_chain :scope_for_create, :acts_as
   end
 
-  module QueryMethods
-    def where_with_acts_as(opts = :chain, *rest)
+  module WhereWithActAs
+    def where(opts = :chain, *rest)
       if acting_as? && opts.is_a?(Hash)
         opts, acts_as_opts = opts.stringify_keys.partition { |k, _v| attribute_method?(k) }
         opts = Hash[opts]
@@ -19,7 +18,14 @@ module ActiveRecord
         opts[acting_as_model.table_name] = acts_as_opts unless acts_as_opts.empty?
       end
       where_without_acts_as(opts, *rest)
-        end
-    alias_method_chain :where, :acts_as
+    end
+  end
+
+  class Relation
+    prepend ScopeForCreateWithActAs
+  end
+
+  module QueryMethods
+    prepend WhereWithActAs
   end
 end
