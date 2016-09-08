e61ea582ba898e335d2e344b054f69fd93edd84d
Nick Landgrebe <nick@landgre.be>
2016-09-08 10:33:34 -0500

    Convert alias_method_chain references to instead use prepend

--- lib/active_record/acts_as/querying.rb
+++ lib/active_record/acts_as/querying.rb
@@ -1,25 +1,28 @@
-
 module ActiveRecord
-  module QueryMethods
-    def where_with_acts_as(opts = :chain, *rest)
-      if acting_as? && opts.is_a?(Hash)
-        opts, acts_as_opts = opts.stringify_keys.partition { |k,v| attribute_method?(k) }
-        opts, acts_as_opts = Hash[opts], Hash[acts_as_opts]
-        opts[acting_as_model.table_name] = acts_as_opts unless acts_as_opts.empty?
+  module ActsAs
+    module QueryMethods
+      def where(opts = :chain, *rest)
+        if acting_as? && opts.is_a?(Hash)
+          opts, acts_as_opts = opts.stringify_keys.partition { |k,v| attribute_method?(k) }
+          opts, acts_as_opts = Hash[opts], Hash[acts_as_opts]
+          opts[acting_as_model.table_name] = acts_as_opts unless acts_as_opts.empty?
+        end
+
+        super
       end
-      where_without_acts_as(opts, *rest)
     end
-    alias_method_chain :where, :acts_as
+
+    module ScopeForCreate
+      def scope_for_create
+        @scope_for_create ||= if acting_as?
+          where_values_hash.merge(where_values_hash(acting_as_model.table_name)).merge(create_with_value)
+        else
+          where_values_hash.merge(create_with_value)
+        end
+      end
+    end
   end
 
-  class Relation
-    def scope_for_create_with_acts_as
-      @scope_for_create ||= if acting_as?
-        where_values_hash.merge(where_values_hash(acting_as_model.table_name)).merge(create_with_value)
-      else
-        where_values_hash.merge(create_with_value)
-      end
-    end
-    alias_method_chain :scope_for_create, :acts_as
-  end
+  Relation.send(:prepend, ActsAs::QueryMethods)
+  Relation.send(:prepend, ActsAs::ScopeForCreate)
 end
