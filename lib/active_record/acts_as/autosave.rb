1b3e8dc2395ed1799f5aed60e70eb2a1c6ee07ce
WANG QIANG <rovingbreeze@gmail.com>
2017-10-16 14:41:53 +0800

    Ensure that `non_cyclic_save` does not get called for a new_record

--- lib/active_record/acts_as/autosave.rb
+++ lib/active_record/acts_as/autosave.rb
@@ -1,8 +1,27 @@
 module ActiveRecord
   module ActsAs
     module Autosave
+      def self.included(base)
+        base.class_eval do
+          # Rails 5 has an issue that after_update callback gets called for new records. The below callbacks ensures
+          # that `non_cyclic_save` is not called in such cases.
+          before_create do
+            @_was_new_record = true
+            true
+          end
+
+          after_rollback do
+            @_was_new_record = nil
+          end
+
+          after_commit do
+            @_was_new_record = nil
+          end
+        end
+      end
+
       def non_cyclic_save(target, &block)
-        return if !target || target.saved_state_set?
+        return if !target || target.saved_state_set? || was_new_record?
 
         @_saved_state = true
         target.instance_variable_set(:@_saved_state, true)
@@ -15,6 +34,11 @@ module ActiveRecord
       def saved_state_set?
         defined?(@_saved_state) ? @_saved_state : false
       end
+
+      def was_new_record?
+        !!@_was_new_record
+      end
+      private :was_new_record?
     end
   end
 end
--- lib/active_record/acts_as/version.rb
+++ lib/active_record/acts_as/version.rb
@@ -3,4 +3,3 @@ module ActiveRecord
     VERSION = "3.0.1"
   end
 end
-
